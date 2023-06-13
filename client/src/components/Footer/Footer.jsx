5557c1b372f3fc996145e8e7c7e4e4a926dbf06b
alimdev <alimwebdev@gmail.com>
2023-06-13 11:50:34 +0400

    build footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            About info
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Contact Info
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Menstyle</span>
+          <span className="copyright">
+            @ Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right"/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer
