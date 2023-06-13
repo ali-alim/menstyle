14aa71ecbc6386c83b386e6a2604d1b98f64abc7
alimdev <alimwebdev@gmail.com>
2023-06-13 11:33:35 +0400

    build navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./navbar.scss"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='wrapper'>
-            <div className='left'></div>
-            <div className='center'></div>
-            <div className='right'></div>
+            <div className='left'>
+              <div className='item'>
+                <img  src="/images/geo.png" width="40px" height="35px" alt="georgian flag" />
+                <KeyboardArrowDownIcon />
+              </div>
+              <div className='item'>
+                <span>USD</span>
+                <KeyboardArrowDownIcon />
+              </div>
+              <div className='item'>
+                <Link className='link' to="products/1">Men</Link>
+              </div>
+              <div className='item'>
+                <Link className='link' to="products/2">Accessories</Link>
+              </div>
+            </div>
+            <div className="center">
+              <Link className ="link" to="/">MENSTYLE</Link>
+            </div>
+            <div className="right">
+              <div className="item">
+                <Link className ="link" to="/">Homepage</Link>
+              </div>
+              <div className="item">
+                <Link className ="link" to="/">About</Link>
+              </div>
+              <div className="item">
+                <Link className ="link" to="/">Contact</Link>
+              </div>
+              <div className="item">
+                <Link className ="link" to="/">Stores</Link>
+              </div>
+              <div className="icons">
+                <SearchIcon/>
+                <PersonOutlineOutlinedIcon/>
+                <FavoriteBorderOutlinedIcon/>
+                <div className="cartIcon">
+                  <ShoppingCartOutlinedIcon/>
+                  <span>0</span>
+                </div>
+              </div>
+            </div>
         </div>
     </div>
   )
